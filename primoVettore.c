e1155edc95a41fad58c9c2747dcd2b51d3c7a604
Paolo Rampino <liadserv@gmail.com>
2024-06-06 10:04:47 +0200

    up

---
+++ primoVettore.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void leggivettore (int *ptr){
+    for (int i=0; i<10;i++){
+        printf("\nElemento %d del vettore: %d", i, *ptr);
+        ptr++;
+    }
+    printf("\nFINE!\n");
+}
+
+void scrivivettore (int *ptr) {
+    for (int i=0; i<10;i++){
+        printf ("\ninserisci un numero: ");
+        scanf("%d", ptr);
+        ptr++;
+    }
+}
+
+int main() {
+    int n[10] = {0};
+    int *n_ptr;
+    n_ptr=&n[0];
+
+    scrivivettore (n_ptr);
+    leggivettore(n_ptr);
+}
