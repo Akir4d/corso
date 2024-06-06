256253d17e2268139ba9dba5289a88b01db7a21d
Paolo Rampino <liadserv@gmail.com>
2024-06-06 10:46:10 +0200

    up

---
+++ variabili.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int intero = 10;
+    float reale = 3.14;
+    char carattere = 'A';
+
+    printf("Valore intero: %d\n", intero);
+    printf("Valore float: %.2f\n", reale);
+    printf("Carattere: %c\n", carattere);
+}
