10f80cf9409a6f8b46174ea9ef74c31266bec26a
Paolo Rampino <liadserv@gmail.com>
2024-06-06 12:59:26 +0200

    ttt

--- primoVettore.c
+++ primoVettore.c
@@ -3,6 +3,7 @@
 void leggivettore (int *ptr){
     for (int i=0; i<10;i++){
         printf("\nElemento %d del vettore: %d", i, *ptr);
+        printf("\nmemoria 0x%x=%d", ptr, *ptr);
         ptr++;
     }
     printf("\nFINE!\n");
@@ -12,6 +13,8 @@ void scrivivettore (int *ptr) {
     for (int i=0; i<10;i++){
         printf ("\ninserisci un numero: ");
         scanf("%d", ptr);
+        
+        //n[2]
         ptr++;
     }
 }
@@ -21,6 +24,7 @@ int main() {
     int *n_ptr;
     n_ptr=&n[0];
     //scrivivettore(&n[0]);
+    
     scrivivettore (n_ptr);
     leggivettore(n_ptr);
 }
